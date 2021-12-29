3c0ff27f09b897d6906ad51e2396dda4f2407e72
sumisu-mitani <charcoal.v.23824@gmail.com>
2021-12-29 18:04:07 +0900

    add base files

---
+++ ms_to_pdf/pdf/pdf/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace pdf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "PDF変換";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(149, 37);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(450, 100);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            this.listBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "作業ファイル（docx,doc,xlsx.xls,pptx,ppt）";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(147, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "保存フォルダ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(149, 157);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(370, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBox1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBox1_DragEnter);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(525, 155);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(74, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "フォルダ";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label_test";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(547, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(52, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(13, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(128, 37);
+            this.panel1.TabIndex = 9;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(16, 18);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "表示用画質";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(16, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(83, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "印刷用画質";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(29, 159);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 188);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ ms_to_pdf/pdf/pdf/Form1.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace pdf
+{
+
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            radioButton2.Checked = true;
+        }
+
+        private bool chk_ms_file(string file_name)
+        {
+            bool wk_flg = false;
+            string wk_file_category = System.IO.Path.GetExtension(file_name);
+
+            if (wk_file_category == (".doc")|| wk_file_category == (".docx")
+                || wk_file_category == (".xls") || wk_file_category == (".xlsx")
+                || wk_file_category == (".ppt") || wk_file_category == (".pptx"))
+            {
+                wk_flg = true;
+            }
+            return (wk_flg);
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            string wk_file_category;
+            foreach(string item in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                if (System.IO.File.GetAttributes(item) == System.IO.FileAttributes.Directory)
+                {
+                    string[] files = System.IO.Directory.GetFiles(item);
+                    foreach(string file in files)
+                    {
+                        wk_file_category = System.IO.Path.GetExtension(file);
+                        if (chk_ms_file(file) == true)
+                        {
+                            listBox1.Items.Add(file);
+                        }
+                    }
+                }
+                else
+                {
+                    wk_file_category = System.IO.Path.GetExtension(item);
+                    if (chk_ms_file(item) == true)
+                    {
+                        listBox1.Items.Add(item);
+                    }
+                }
+            }
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Delete)
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.FileName = "SelectFolder";
+            op.Filter = "Folder|.";
+            op.CheckFileExists = false;
+
+            
+            DialogResult result = op.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                textBox1.Text = System.IO.Path.GetDirectoryName(op.FileName);
+            }
+        }
+
+        private void doc2pdf(string doc_file)
+        {
+            Microsoft.Office.Interop.Word.Application objWord = null;
+            Microsoft.Office.Interop.Word.Documents objWordDocuments = null;
+            Microsoft.Office.Interop.Word.Document objWordDocument = null;
+
+            string strWordFilePath = string.Empty;
+            string strPdfFilePath = string.Empty;
+
+            try
+            {
+                strWordFilePath = doc_file;
+                strPdfFilePath = System.IO.Path.GetDirectoryName(strWordFilePath) + @"/" + System.IO.Path.GetFileNameWithoutExtension(strWordFilePath) + @".pdf";
+                objWord = new Microsoft.Office.Interop.Word.Application();
+                objWordDocuments = objWord.Documents;
+                objWordDocument = objWordDocuments.Open(strWordFilePath);
+
+                objWordDocument.ExportAsFixedFormat
+                    (strPdfFilePath,
+                    Microsoft.Office.Interop.Word.WdExportFormat.wdExportFormatPDF,
+                    false,
+                    Microsoft.Office.Interop.Word.WdExportOptimizeFor.wdExportOptimizeForPrint,
+                    Microsoft.Office.Interop.Word.WdExportRange.wdExportAllDocument,
+                    1, 1,
+                    Microsoft.Office.Interop.Word.WdExportItem.wdExportDocumentContent,
+                    false, true,
+                    Microsoft.Office.Interop.Word.WdExportCreateBookmarks.wdExportCreateHeadingBookmarks);
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (objWordDocument != null)
+                {
+                    objWordDocument.Close();
+                }
+                if (objWord != null)
+                {
+                    objWord.Quit();
+                }
+            }
+        }
+        private void xls2pdf(string xls_file)
+        {
+            Microsoft.Office.Interop.Excel.Application objExcel = null;
+            Microsoft.Office.Interop.Excel.Workbooks objExcelWorkbooks = null;
+            Microsoft.Office.Interop.Excel.Workbook objExcelWorkbook = null;
+
+            string strExcelFilePath = xls_file;
+            string strPdfFilePath = string.Empty;
+            try
+            {
+                strPdfFilePath = System.IO.Path.GetDirectoryName(strExcelFilePath) + @"/" + System.IO.Path.GetFileNameWithoutExtension(strExcelFilePath) + @".pdf";
+                objExcel = new Microsoft.Office.Interop.Excel.Application();
+                objExcelWorkbooks = objExcel.Workbooks;
+                objExcelWorkbook = objExcelWorkbooks.Open(strExcelFilePath);
+
+                objExcelWorkbook.ExportAsFixedFormat(
+                    Microsoft.Office.Interop.Excel.XlFixedFormatType.xlTypePDF,
+                    strPdfFilePath,
+                    Microsoft.Office.Interop.Excel.XlFixedFormatQuality.xlQualityStandard,
+                    true,
+                    false,
+                    Type.Missing,
+                    Type.Missing,
+                    false,
+                    Type.Missing
+                    );
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (objExcelWorkbook != null)
+                {
+                    objExcelWorkbook.Close();
+                }
+                if (objExcel != null)
+                {
+                    objExcel.Quit();
+                }
+            }
+        }
+
+        private void ppt2pdf(string xls_file)
+        {
+            Microsoft.Office.Interop.PowerPoint.Application objPPT = null;
+            Microsoft.Office.Interop.PowerPoint.Presentations objPPTPresentations = null;
+            Microsoft.Office.Interop.PowerPoint.Presentation objPPTPresentation = null;
+
+            string strPPTFilePath = xls_file;
+            string strPdfFilePath = string.Empty;
+            try
+            {
+                strPdfFilePath = System.IO.Path.GetDirectoryName(strPPTFilePath) + @"/" + System.IO.Path.GetFileNameWithoutExtension(strPPTFilePath) + @".pdf";
+                objPPT = new Microsoft.Office.Interop.PowerPoint.Application();
+                objPPTPresentations = objPPT.Presentations;
+                objPPTPresentation = objPPTPresentations.Open(strPPTFilePath);
+
+                objPPTPresentation.ExportAsFixedFormat(
+                    strPdfFilePath,
+                    Microsoft.Office.Interop.PowerPoint.PpFixedFormatType.ppFixedFormatTypePDF,
+                    Microsoft.Office.Interop.PowerPoint.PpFixedFormatIntent.ppFixedFormatIntentPrint
+                   );
+
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                if (objPPTPresentation != null)
+                {
+                    objPPTPresentation.Close();
+                }
+                if (objPPT != null)
+                {
+                    objPPT.Quit();
+                }
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string wk_file = listBox1.Items.IndexOf(1).ToString();
+            string wk_file_path;
+            string wk_file_category;
+            if ((textBox1.Text != null)&& (listBox1.Items.Count != 0))
+            {
+                for(int i = 0; i < listBox1.Items.Count; i++)
+                {
+                    wk_file_category = System.IO.Path.GetExtension(listBox1.Items[i].ToString());
+                    wk_file_path = @listBox1.Items[i].ToString();
+                    if ((wk_file_category == ".docx")|| (wk_file_category == ".doc"))
+                    {
+                        doc2pdf(wk_file_path);                    
+                    }else if((wk_file_category == ".xls")||(wk_file_category == ".xlsx"))
+                    {
+                        xls2pdf(wk_file_path);
+                    }else if((wk_file_category == ".ppt") || (wk_file_category == ".pptx"))
+                    {
+                        ppt2pdf(wk_file_path);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            foreach (string item in (string[])e.Data.GetData(DataFormats.FileDrop))
+            {
+                if (System.IO.File.GetAttributes(item) == System.IO.FileAttributes.Directory)
+                {
+                    textBox1.Text = item;
+                }
+            }
+        }
+        private void listBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void textBox1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
